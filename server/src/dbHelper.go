c46997a34cd78044a12bb40ee377ef14906b1bf5
Mark Dewey <mdewey@problemsolutions.net>
2014-06-30 16:38:10 -0400

    added  get game method

--- server/src/dbHelper.go
+++ server/src/dbHelper.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	//"fmt"
+	// "fmt"
+	"container/list"
 	"github.com/jmcvetta/neoism"
 	//"code.google.com/p/go-uuid/uuid"
 )
@@ -22,6 +23,7 @@ type Game struct {
 	latitude  float64
 	longitude float64
 	isActive  bool
+	Id string
 }
 
 func AddUser(username string, name string, lat float64, lon float64) {
@@ -38,12 +40,12 @@ func AddUser(username string, name string, lat float64, lon float64) {
 func AddGame(name string, lat float64, lon float64) {
 
 	db, _ := neoism.Connect(connectionString)
-	// fmt.Println(db)
 	node, _ := db.CreateNode(neoism.Props{"name": name, "latitude": lat, "longitude": lon, "Id": "some guid here", "isActive": true})
 	node.AddLabel("Game")
+
 }
 
-func GetAllGames(){
+func GetAllGames() *list.List {
 	db, _ := neoism.Connect(connectionString)
 	result := []struct {
 		N neoism.Node // Column "n" gets automagically unmarshalled into field N
@@ -52,11 +54,35 @@ func GetAllGames(){
 		Statement: `
 			MATCH (n:Game)
 			RETURN n`,
+		Result: &result,
+	}
+	db.Cypher(&query)
+	rv := list.New()
+	for _, element := range result {
+		//TODO: turn this into a game
+		rv.PushFront(element.N.Data)
+	}
+	return rv
+}
+
+func GetGame(id string) Game {
+	db, _ := neoism.Connect(connectionString)
+	result := []struct {
+		N neoism.Node // Column "n" gets automagically unmarshalled into field N
+	}{}
+	query := neoism.CypherQuery{
+		Statement: `
+			MATCH (n:Game)
+			WHERE n.Id = {id}
+			RETURN n`,
+		Parameters: neoism.Props{"id": id},
 		Result:     &result,
 	}
 	db.Cypher(&query)
-	fmt.Println(result)
+	rv := Game{result[0].N.Data["name"].(string), result[0].N.Data["latitude"].(float64), result[0].N.Data["longitude"].(float64), result[0].N.Data["isActive"].(bool),result[0].N.Data["Id"].(string)}
+	return rv
 }
+
 
 func GetUser(username string) User {
 	db, _ := neoism.Connect(connectionString)
--- server/src/main.go
+++ server/src/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 
 // "github.com/jmcvetta/neoism"
 )
 
 func main() {
-	userName := "userNaming4"
+	//	userName := "userNaming4"
 
-	AddUser(userName, "full naming", 44.4, 55.5)
-	user := GetUser(userName)
-	fmt.Println(user)
+	//AddUser(userName, "full naming", 44.4, 55.5)
+	// user := GetUser(userName)
+	// fmt.Println(user)
+	//AddGame("New Game 2", 43.2, 54.5)
+	//all := GetAllGames()
+	//fmt.Println(all)
+	//PrintOutList(all)
 
-	AddGame("New Game", 43.2, 54.5)
-	GetAllGames()
+	game := GetGame("some guid here")
+	fmt.Println(game)
+
+}
+
+func PrintOutList(l *list.List) {
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println(e)
+	}
 }
