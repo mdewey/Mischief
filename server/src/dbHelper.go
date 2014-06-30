3e1455433eb314ba50cccc5c662cbd2c523f7e84
Mark Dewey <mdewey@problemsolutions.net>
2014-06-29 22:36:30 -0400

    init commit and add user is done

--- server/src/dbHelper.go
+++ server/src/dbHelper.go
@@ -5,9 +5,15 @@ import (
 	"github.com/jmcvetta/neoism"
 )
 
+var db Database
+
+func ConnectToDb(){
+	db, _ := neoism.Connect("http://localhost:7474/db/data")
+}
+
 func AddUser(username string, name string, lat float64, lon float64) {
 
-	db, _ := neoism.Connect("http://localhost:7474/db/data")
+	ConnectToDb()
 	// fmt.Println(db)	
 	node, _  := db.CreateNode(neoism.Props{"name":name, "username":username, "latitude":lat, "longitude": lon})
 	node.AddLabel("User")
@@ -17,5 +23,13 @@ func AddUser(username string, name string, lat float64, lon float64) {
 }
 
 func GetUser(username string){
-	
+	ConnectToDb()
+	query := neoism.CypherQuery{
+		Statement: `
+			MATCH (n:Person)
+			WHERE n.username = {uname}
+			RETURN n`,
+		Parameters: neoism.Props{"uname":username}	
+	}
+
 }
